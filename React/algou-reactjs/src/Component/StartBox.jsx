9d0d7e11f131db282ef0815c7239f623fa589ea3
RajeshKumarSinha <jaibesh30.2004@gmail.com>
2025-06-17 22:45:48 +0530

    2nd commit

--- React/algou-reactjs/src/Component/StartBox.jsx
+++ React/algou-reactjs/src/Component/StartBox.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 function StartBox() {
   return (
     <>
-    <div className = "w-56 h-24 bg--400 bg-opacity-30 rounded-lg shadow-md bg-gradient-to-r from-cyan-400 via-red-300">
-        </div>
+    <div className = "">
+    <div className = "w-2/5 h-96 ml-[600px] mt-[100px] bg-opacity-30 rounded-lg border-b-gray-500 border-4 border-solid shadow-2xl shadow-neutral-500 animated-gradient grid grid-rows-4 place-items-center">
+      <button className = "w-1/5 h-10 bg-gradient-to-r from-[#e0e0e0] via-[#bdbdbd] to-[#757575] shadow-lg row-start-2 rounded-lg">
+        Login
+      </button>
+      <button className = "w-1/5 h-10 bg-gradient-to-r from-[#e0e0e0] via-[#bdbdbd] to-[#757575] shadow-lg row-start-3 rounded-lg">
+        Sign-Up
+      </button>
+    </div>
+    </div>
     </>
   )
 }
--- React/algou-reactjs/src/service/api.js
+++
@@ -1,2 +0,0 @@
-import axios from 'axios';
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ;
